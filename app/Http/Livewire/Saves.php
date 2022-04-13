9dd5393ab52e4823a866cb0686d2b2fe608c4a49
kratos <desmond8500@gmail.com>
2022-04-13 11:50:53 +0000

    updates

--- app/Http/Livewire/Saves.php
+++ app/Http/Livewire/Saves.php
@@ -10,7 +10,15 @@ class Saves extends Component
     public function render()
     {
         return view('livewire.saves',[
-            'fichiers' => Storage::disk('public')->files('saves')
+            // 'fichiers' => Storage::disk('public')->files('saves')
+            'fichiers' => glob('./storage/saves/*')
         ])->extends('layout.app')->section('content');
+    }
+
+    public $file;
+
+    public function download($file)
+    {
+        Storage::disk('public')->download($file);
     }
 }
